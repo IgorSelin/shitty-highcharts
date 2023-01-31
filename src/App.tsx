3c79c050333c1b954c395172f352641c90212d33
Ihor Selin <ise@interlogic.com.ua>
2023-01-31 15:44:44 +0200

    IS-1 added issue code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Select } from "antd";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { useState } from "react";
+import style from "./UnitsReport.module.scss";
 
-function App() {
+const App = () => {
+  const [selectedUnit, setSelectedUnit] = useState("Unit 1");
+
+  const mockUnits = [
+    { value: "Unit 1" },
+    { value: "Unit 2" },
+    { value: "Unit 3" },
+  ];
+
+  const options = {
+    chart: {
+      type: "column",
+      width: 500,
+      height: 350,
+    },
+    title: {
+      text: "",
+    },
+    xAxis: {
+      categories: ["", "", ""],
+    },
+    yAxis: {
+      title: {
+        text: "",
+      },
+    },
+    plotOptions: {
+      series: {
+        stacking: "normal",
+        pointWidth: 50,
+      },
+    },
+    series: [
+      {
+        name: "New",
+        data: [10],
+      },
+      {
+        name: "Reviewed",
+        data: [5],
+      },
+      {
+        name: "Amended",
+        data: [8],
+      },
+      {
+        name: "Reviewing",
+        data: [5],
+      },
+    ],
+    credits: {
+      enabled: false,
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <div className={style.infoBlock}>
+        <div className={style.selectContainer}>
+          Select unit:
+          <Select
+            placeholder="Unit"
+            defaultValue={selectedUnit}
+            options={mockUnits}
+            onChange={(value) => setSelectedUnit(value)}
+          />
+        </div>
+        <div className={style.info}>
+          <div className={style.title}> {selectedUnit}:</div>
+          <div className={style.count}>20 submissions</div>
+          <div className={style.infoItem}>
+            Reviewed per months:<span>10</span>
+          </div>
+          <div className={style.infoItem}>
+            Reviewed per year:<span>15</span>
+          </div>
+          <div className={style.infoItem}>
+            Approval times:<span>7 days(in average)</span>
+          </div>
+        </div>
+      </div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
-}
+};
 
 export default App;
+
